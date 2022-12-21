5fb573af6aa27712ffa64dfb55c3be35824d95ea
Abhishek Kumar <sahniak56@gmail.com>
2022-12-22 01:36:46 +0530

    g

--- src/components/Header/Headers.jsx
+++ src/components/Header/Headers.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useNavigate } from 'react-router-dom';
 
 const Headers = () => {
+  const navigate=useNavigate()
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -11,9 +14,8 @@ const Headers = () => {
           <Navbar.Brand href="#home">Technical Abhi</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="/register">Register</Nav.Link>
-
           </Nav>
+          <Button variant="outline-success" onClick={()=>navigate('/register')}>Register</Button>
         </Container>
       </Navbar>
     </>
